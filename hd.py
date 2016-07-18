8311b0cfb01a89a7333623f4b0320997d3db9e26
budleigh <arguewithplato@gmail.com>
2016-07-17 23:48:08 -0700

    refactor

--- hd.py
+++ hd.py
@@ -1,11 +1,7 @@
-from flask import Flask, render_template
-
+from flask import Flask
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+import src.server.views
 
 if __name__ == '__main__':
     app.run()
