8fc1be6e7efa18fc22b5096bd524365f5177c470
budleigh <arguewithplato@gmail.com>
2016-07-18 13:54:17 -0700

    add menu js

---
+++ src/client/menu.js
@@ -0,0 +1,27 @@
+module.exports = [
+    {
+        name: 'Cinnamon bombs',
+        price: 2,
+        quantity: 2
+    },
+    {
+        name: 'Almond brulee',
+        price: 2,
+        quantity: 2
+    },
+    {
+        name: 'Cranstachio bars',
+        price: 2,
+        quantity: 4
+    },
+    {
+        name: 'Granola crunch',
+        price: 4,
+        quantity: 2
+    },
+    {
+        name: 'Cider shortbread',
+        price: 2,
+        quantity: 2
+    }
+]
